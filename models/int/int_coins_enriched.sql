ebbda70fe449b2513be812ca709d20d2fb22b90e
nataraj.h@northeastern.edu <nataraj.h@northeastern.edu>
2025-09-25 04:05:09 +0000

    changed int queries

--- models/int/int_coins_enriched.sql
+++ models/int/int_coins_enriched.sql
@@ -1,36 +1,25 @@
-{{ config(
-    materialized='incremental'
-) }}
+{{ config(materialized='incremental') }}
 
-with markets as (
-  select * from {{ ref('stg_coins_markets') }}
-),
-
-trending as (
-  select coin_id, score, ingested_at as trending_ingested_at
-  from {{ ref('stg_coins_trending') }}
+with latest as (
+    select coalesce(max(ingested_at), '1970-01-01'::timestamp_ntz) as max_ingested
+    from {{ this }}
 )
 
-select
-  m.coin_id,
-  m.symbol,
-  m.name,
-  m.current_price,
-  m.market_cap,
-  m.total_volume,
-  m.price_change_pct_24h,
-  m.market_cap_rank,
-  m.all_time_high,
-  m.all_time_low,
-  -- trending enrichment: if there's a trending record at same ingest time (or nearest)
-  case when t.coin_id is not null then true else false end as is_trending,
-  t.score as trending_score,
-  greatest(m.ingested_at, t.trending_ingested_at) as ingested_at
-from markets m
-left join trending t
-  on m.coin_id = t.coin_id
-  and t.trending_ingested_at = m.ingested_at  -- simple alignment by time
+, coins as (
+    select
+        coin_id,
+        name,
+        symbol,
+        category_name,
+        current_price,
+        price_change_pct_24h,
+        market_cap,
+        total_volume,
+        ingested_at
+    from {{ ref('stg_coins_markets') }}
+    {% if is_incremental() %}
+    where ingested_at > (select max_ingested from latest)
+    {% endif %}
+)
 
-{% if is_incremental() %}
-where ingested_at > (select coalesce(max(ingested_at), '1970-01-01'::timestamp_ntz) from {{ this }})
-{% endif %}
+select * from coins
