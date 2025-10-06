f420809a1c3973c9559e5b0f9249e7f058ccb877
nataraj.h@northeastern.edu <nataraj.h@northeastern.edu>
2025-10-06 03:20:42 +0000

    fixed mart syntax

--- models/mart/mart_top_movers.sql
+++ models/mart/mart_top_movers.sql
@@ -6,7 +6,7 @@
 
 
 -- Filter new data from intermediate layer
-base as (
+with base as (
     select *
     from {{ ref('int_coins_enriched') }}
     {% if is_incremental() %}
@@ -14,7 +14,7 @@ base as (
       select coalesce(max(fetched_at), '1970-01-01'::timestamp_ntz) from {{ this }}
   )
   {% endif %}
-)
+),
 
 -- Rank movers and add ingestion timestamp
 ranked as (
